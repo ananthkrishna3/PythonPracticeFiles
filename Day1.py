45114f337ae5c915f44c5d77d953b11851137603
ananthkrishna3 <ananthkrishna2112@gmail.com>
2019-07-23 22:42:32 +0530

    Add files via upload
    Python practice files

---
+++ Day1.py
@@ -0,0 +1,11 @@
+a = 10
+b = 20
+c= a + b
+print(c);
+x = 3.14
+print(x);
+y = int(x)
+print(y)
+b =int(2000*(20/100))
+print(b);
+
---
+++ Lists Practice.py
@@ -0,0 +1,24 @@
+dynamiclist= [1,"hello",3.14]
+#y = len(dynamiclist)
+#print(y);
+#for x in range(1,y):
+  # print(dynamiclist[x])
+#Sorting List
+sortList = [8,6,93,41,3,8,80,16,18,20,41,50]
+print(sortList)
+for n in range(len(sortList)):
+    for x in range(len(sortList)):
+        if sortList[n] < sortList[x]:
+                temp=sortList[n]
+                sortList[n]=sortList[x]
+                sortList[x] = temp
+for m in range(len(sortList)):
+    print(sortList[m])
+
+#adding Elements in Lists
+ListExample = [1,3,5,7,9]
+ListExample.append(1)
+ListExample.insert(1,2)
+Tuples=(10,11)
+ListExample.insert(7,Tuples)
+print(ListExample)
